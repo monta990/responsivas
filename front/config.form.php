0132e397f527f57f29be2c1db505e626cd6583a6
Edwin <elias986@yahoo.com>
2026-08-18 22:17:18 -0700

    Delete front directory

--- front/config.form.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('GLPI_ROOT')) {
-    die('Direct access not allowed');
-}
-require_once dirname(__DIR__) . '/inc/paths.class.php';
-require_once dirname(__DIR__) . '/inc/config.class.php';
